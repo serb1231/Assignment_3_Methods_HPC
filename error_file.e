eaf28dbf1108f1452559db521df4771801c930fc
Serban-Mihai Ionescu <smio@login1.hsn.dardel.pdc.kth.se>
2026-05-03 17:22:40 +0200

    test on dardel

---
+++ error_file.e
@@ -0,0 +1,1 @@
+/var/spool/slurm/slurmd/job20290387/slurm_script: line 19: ./run_ex3_diff_grid_sizez.sh: No such file or directory
---
+++ scripts/run_peak_flops.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -l
+# The -l above is required to get the full environment with modules
+
+# The name of the script is myjob
+#SBATCH -J myjob
+# 10 minutes wall-clock time will be given to this job
+#SBATCH -t 00:10:00
+#SBATCH -A edu26.DD2356
+# Number of nodes
+#SBATCH -p shared
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=128
+#SBATCH --nodes=1
+#SBATCH -e error_file.e
+
+# Run the executable file 
+# and write the output into my_output_file
+
+./code_par > output_par.txt
